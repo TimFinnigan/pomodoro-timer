f30f6510a23937b32504bf0668f1465b050e00bb
Tim Finnigan <tim.finnigan1@gmail.com>
2020-04-04 10:23:10 -0400

    alert when timer is up

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 $(document).ready(function() {
-  const timerUp = function() {
-    // alert("timer up1");
+  const timesUp = function() {
+    alert("Timer up!");
+    // play bell sound
+    // update pomodors complete in local storage
+    // window.location.reload();
   };
 
-  function formatTime(time) {
-    // Hours, minutes and seconds
+  const formatTime = function(time) {
     const hrs = ~~(time / 3600);
     const mins = ~~((time % 3600) / 60);
     const secs = ~~time % 60;
 
-    // Output like "1:01" or "4:03:59" or "123:03:59"
-    let ret = "";
+    let clock = "";
 
     if (hrs > 0) {
-      ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+      clock += "" + hrs + ":" + (mins < 10 ? "0" : "");
     }
 
-    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
-    ret += "" + secs;
-    if (secs === 0) {
-      timerUp();
-      return false;
-    }
+    clock += "" + mins + ":" + (secs < 10 ? "0" : "");
+    clock += "" + secs;
 
-    return ret;
-  }
+    return clock;
+  };
+
   const countdownNumberEl = document.getElementById("countdown-number");
   const circle = document.getElementsByClassName("countdown-circle")[0];
   const countdown = 5;
   let newcountdown = countdown;
   const maxoffset = 2 * Math.PI * 100;
   let offset = 0;
+
   countdownNumberEl.textContent = formatTime(countdown);
 
   tick = setInterval(function() {
     newcountdown = --newcountdown <= 0 ? 0 : newcountdown;
+
     if (offset - maxoffset / countdown >= -Math.abs(maxoffset)) {
       offset = offset - maxoffset / countdown;
     } else {
@@ -42,7 +42,13 @@ $(document).ready(function() {
       clearInterval(tick);
     }
 
+    if (newcountdown === 0) {
+      timesUp();
+      clearInterval(tick);
+    }
+
     countdownNumberEl.textContent = formatTime(newcountdown);
+
     circle.setAttribute("style", "stroke-dashoffset:" + offset + "px");
   }, 1000);
 });
